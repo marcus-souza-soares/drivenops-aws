fce258469921820d3d856ba185374607abef9fde
Marcus <marcusadrenaline@gmail.com>
2022-10-16 15:06:49 -0300

    fix

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -15,6 +15,7 @@ function App() {
         setStudent(student);
       }
     } catch (error) {
+      console.log(error.response)
       alert('Não foi possível realizar o sorteio!');
     }
   }, []);
